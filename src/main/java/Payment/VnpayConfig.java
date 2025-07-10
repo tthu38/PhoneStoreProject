69d26bb22d0cfb0b3c9363fb24d19d7bd76d5b6d
Lương Đặng Hoàng Lưu <145367573+HoangLuu217@users.noreply.github.com>
2025-07-10 14:56:04 +0700

    vnpay

--- src/main/java/Payment/VnpayConfig.java
+++ src/main/java/Payment/VnpayConfig.java
@@ -21,7 +21,7 @@ import jakarta.servlet.http.HttpServletRequest;
 public class VnpayConfig {
 
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static String vnp_ReturnUrl = "http://localhost:8080/vn_pay/vnpayReturn";
+    public static String vnp_ReturnUrl = "http://localhost:8080/PhoneStore/vnpayreturn";
     public static String vnp_TmnCode = "4YUP19I4";
     public static String secretKey = "MDUIFDCRAKLNBPOFIAFNEKFRNMFBYEPX";
     public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
